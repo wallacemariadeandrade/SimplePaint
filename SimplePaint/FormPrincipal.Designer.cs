0bfa5fd63588740505fa0fee23775e0ab1db1b18
Wallace <66746229+wallacemariadeandrade@users.noreply.github.com>
2020-06-10 19:54:03 -0300

    Adicionados arquivos do projeto

---
+++ SimplePaint/FormPrincipal.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace SimplePaint
+{
+    partial class FormPrincipal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.buttonSalvar = new System.Windows.Forms.Button();
+            this.buttonLimpar = new System.Windows.Forms.Button();
+            this.buttonBorracha = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBoxEspessuraDaCaneta = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonCorCaneta = new System.Windows.Forms.Button();
+            this.panelPintura = new System.Windows.Forms.Panel();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.buttonSalvar);
+            this.groupBox1.Controls.Add(this.buttonLimpar);
+            this.groupBox1.Controls.Add(this.buttonBorracha);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.comboBoxEspessuraDaCaneta);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.buttonCorCaneta);
+            this.groupBox1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.Color.White;
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(811, 88);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Personalizar";
+            // 
+            // buttonSalvar
+            // 
+            this.buttonSalvar.BackColor = System.Drawing.Color.Black;
+            this.buttonSalvar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSalvar.Location = new System.Drawing.Point(697, 32);
+            this.buttonSalvar.Name = "buttonSalvar";
+            this.buttonSalvar.Size = new System.Drawing.Size(92, 33);
+            this.buttonSalvar.TabIndex = 6;
+            this.buttonSalvar.Text = "Salvar";
+            this.buttonSalvar.UseVisualStyleBackColor = false;
+            this.buttonSalvar.Click += new System.EventHandler(this.buttonSalvar_Click);
+            // 
+            // buttonLimpar
+            // 
+            this.buttonLimpar.BackColor = System.Drawing.Color.Black;
+            this.buttonLimpar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonLimpar.Location = new System.Drawing.Point(599, 32);
+            this.buttonLimpar.Name = "buttonLimpar";
+            this.buttonLimpar.Size = new System.Drawing.Size(92, 33);
+            this.buttonLimpar.TabIndex = 5;
+            this.buttonLimpar.Text = "Limpar";
+            this.buttonLimpar.UseVisualStyleBackColor = false;
+            this.buttonLimpar.Click += new System.EventHandler(this.buttonLimpar_Click);
+            // 
+            // buttonBorracha
+            // 
+            this.buttonBorracha.BackColor = System.Drawing.Color.Black;
+            this.buttonBorracha.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonBorracha.Location = new System.Drawing.Point(501, 32);
+            this.buttonBorracha.Name = "buttonBorracha";
+            this.buttonBorracha.Size = new System.Drawing.Size(92, 33);
+            this.buttonBorracha.TabIndex = 4;
+            this.buttonBorracha.Text = "Borracha";
+            this.buttonBorracha.UseVisualStyleBackColor = false;
+            this.buttonBorracha.MouseDown += new System.Windows.Forms.MouseEventHandler(this.buttonBorracha_MouseDown);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(239, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(160, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Espessura da Caneta";
+            // 
+            // comboBoxEspessuraDaCaneta
+            // 
+            this.comboBoxEspessuraDaCaneta.FormattingEnabled = true;
+            this.comboBoxEspessuraDaCaneta.ItemHeight = 18;
+            this.comboBoxEspessuraDaCaneta.Location = new System.Drawing.Point(405, 35);
+            this.comboBoxEspessuraDaCaneta.Name = "comboBoxEspessuraDaCaneta";
+            this.comboBoxEspessuraDaCaneta.Size = new System.Drawing.Size(81, 26);
+            this.comboBoxEspessuraDaCaneta.TabIndex = 2;
+            this.comboBoxEspessuraDaCaneta.SelectedIndexChanged += new System.EventHandler(this.comboBoxEspessuraDaCaneta_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Cor da Caneta";
+            // 
+            // buttonCorCaneta
+            // 
+            this.buttonCorCaneta.BackColor = System.Drawing.Color.Blue;
+            this.buttonCorCaneta.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonCorCaneta.Location = new System.Drawing.Point(141, 31);
+            this.buttonCorCaneta.Name = "buttonCorCaneta";
+            this.buttonCorCaneta.Size = new System.Drawing.Size(75, 33);
+            this.buttonCorCaneta.TabIndex = 0;
+            this.buttonCorCaneta.UseVisualStyleBackColor = false;
+            this.buttonCorCaneta.Click += new System.EventHandler(this.buttonCorCaneta_Click);
+            // 
+            // panelPintura
+            // 
+            this.panelPintura.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelPintura.BackColor = System.Drawing.Color.White;
+            this.panelPintura.Location = new System.Drawing.Point(13, 108);
+            this.panelPintura.Name = "panelPintura";
+            this.panelPintura.Size = new System.Drawing.Size(811, 330);
+            this.panelPintura.TabIndex = 1;
+            this.panelPintura.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelPintura_MouseDown);
+            this.panelPintura.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panelPintura_MouseMove);
+            this.panelPintura.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panelPintura_MouseUp);
+            this.panelPintura.Resize += new System.EventHandler(this.panelPintura_Resize);
+            // 
+            // FormPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(836, 450);
+            this.Controls.Add(this.panelPintura);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "FormPrincipal";
+            this.ShowIcon = false;
+            this.Text = "Simple Paint";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button buttonSalvar;
+        private System.Windows.Forms.Button buttonLimpar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBoxEspessuraDaCaneta;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonCorCaneta;
+        private System.Windows.Forms.Panel panelPintura;
+        private System.Windows.Forms.Button buttonBorracha;
+    }
+}
+
---
+++ SimplePaint/FormPrincipal.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimplePaint
+{
+    public partial class FormPrincipal : Form
+    {
+        private bool flagPintar = false; // Para controlar quando se deve pintar
+        private Graphics graphicsPainelPintura;
+        private float espessuraCaneta;
+        private Color corBorracha;
+        private bool flagApagar = false; // Para controlar quando se deve apagar com a borracha
+        private Image imagemASalvar;
+        private Graphics graphicsImagemASalvar;
+
+
+        public FormPrincipal()
+        {
+            InitializeComponent();
+
+            // As propriedades abaixo só fazem efeito quando o botão está com a propriedade FlatStyle
+            // setada em Flat
+            buttonBorracha.FlatAppearance.MouseOverBackColor = Color.DarkSlateGray; // altera a cor do botão quando o cursor está sobre ele
+            buttonLimpar.FlatAppearance.MouseOverBackColor = Color.DarkSlateGray; // altera a cor do botão quando o cursor está sobre ele
+            buttonSalvar.FlatAppearance.MouseOverBackColor = Color.DarkSlateGray; // altera a cor do botão quando o cursor está sobre ele
+
+            for (int i=2; i <= 100; i+=2) // Irá preencher a comboBox de 2 até 100 contando de 2 em 2
+            {
+                comboBoxEspessuraDaCaneta.Items.Add(i);
+            }
+            comboBoxEspessuraDaCaneta.Text = "10"; // Texto que irá aparecer inicialmente na comboBox
+            comboBoxEspessuraDaCaneta.IntegralHeight = false; // Necesário para que seja possível definir a quantidade de itens a serem exibidos na combobox
+            comboBoxEspessuraDaCaneta.MaxDropDownItems = 5; // Define o número de itens a serem exibidos na combobox
+
+            graphicsPainelPintura = panelPintura.CreateGraphics(); // O graphics permite o desenho sobre o controle
+            espessuraCaneta = float.Parse(comboBoxEspessuraDaCaneta.Text); // Converte o texto da comboBox espessura para tipo float
+            corBorracha = panelPintura.BackColor; // Especifica a cor padrão da borracha como a cor de fundo do painel
+
+            imagemASalvar = new Bitmap(panelPintura.Width, panelPintura.Height); // imagem para salvar
+            graphicsImagemASalvar = Graphics.FromImage(imagemASalvar); // extraindo graphics da imagem para salvar de forma a podermos desenhar nela
+            graphicsImagemASalvar.Clear(panelPintura.BackColor); // Preenchemos a imagem com a cor do fundo do painel
+        }
+
+        // Handler do evento de click do botão
+        // Serve para manipularmos o que acontece quando o clique ocorre
+        private void buttonCorCaneta_Click(object sender, EventArgs e)
+        {
+            var colorDialog = new ColorDialog(); // ColorDialog é uma caixa de seleção de cores
+            var corEscolhida = colorDialog.ShowDialog(); // Exibe na forma modal - aplicação fica travada enquanto o diálogo não é resolvido
+            if(corEscolhida == DialogResult.OK) // Veriica se usuário clicou mesmo em OK
+            {
+                buttonCorCaneta.BackColor = colorDialog.Color; // Alteramos a cor do botão para a cor escolhida pelo usuário
+            }
+        }
+
+        private void panelPintura_MouseDown(object sender, MouseEventArgs e)
+        {
+            flagPintar = true; // Quando usuário pressiona o botão do mouse sob o painel entendemos que ele quer pintar
+        }
+
+        private void panelPintura_MouseUp(object sender, MouseEventArgs e)
+        {
+            flagPintar = false; // Quando usuário solta o botão do mouse entendemos que ele não quer pintar
+        }
+
+        private void panelPintura_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(flagPintar) // Só pintamos se o botão do mouse estiver pressionado! Como sabemos isso? Através da flag Pintar
+            {
+                if(!flagApagar)
+                {
+                    // Desenhamos uma elipse de cor e espessura definida pelo usuário, nas coordenadas do ponteiro do mouse
+                    graphicsPainelPintura
+                        .DrawEllipse(new Pen(buttonCorCaneta.BackColor, espessuraCaneta), new RectangleF(e.X, e.Y, espessuraCaneta, espessuraCaneta));
+                    // Desenhando na imagem para salvar
+                    graphicsImagemASalvar
+                        .DrawEllipse(new Pen(buttonCorCaneta.BackColor, espessuraCaneta), new RectangleF(e.X, e.Y, espessuraCaneta, espessuraCaneta));
+                }
+                else
+                {
+                    // Desenhamos uma elipse de cor e espessura definida pelo usuário, nas coordenadas do ponteiro do mouse
+                    graphicsPainelPintura
+                        .DrawRectangle(new Pen(corBorracha, espessuraCaneta), new Rectangle(e.X, e.Y, (int)espessuraCaneta, (int)espessuraCaneta)); // Transformamos uma variável tipo float em tipo inteiro
+                    // Desenhando na imagem para salvar
+                    graphicsImagemASalvar
+                        .DrawRectangle(new Pen(corBorracha, espessuraCaneta), new Rectangle(e.X, e.Y, (int)espessuraCaneta, (int)espessuraCaneta)); // Transformamos uma variável tipo float em tipo inteiro
+                }
+            }
+        }
+
+        private void buttonLimpar_Click(object sender, EventArgs e)
+        {
+            // A MessageBox representa uma janela para exibição de mensagens ao usuário
+            if(MessageBox.Show("Tem certeza disso? Todo o desenho será apagado!", "Apagar desenho", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning) == DialogResult.OK)
+            {
+                graphicsPainelPintura.Clear(Color.White); // Limpa todo o desenho e preenche novamente o fundo do painel de branco
+                imagemASalvar = new Bitmap(panelPintura.Width, panelPintura.Height); // imagem para salvar
+                graphicsImagemASalvar = Graphics.FromImage(imagemASalvar); // extraindo graphics da imagem para salvar de forma a podermos desenhar nela
+                graphicsImagemASalvar.Clear(panelPintura.BackColor); // Preenchemos a imagem com a cor do fundo do painel
+            }
+        }
+
+        // SelectedIndexChanged ocorre quando é feita uma escolha de item pelo usuário na comboBox
+        private void comboBoxEspessuraDaCaneta_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            espessuraCaneta = float.Parse(comboBoxEspessuraDaCaneta.SelectedItem.ToString());
+        }
+
+        private void buttonBorracha_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right) // Se clicar com botão direito do mouse sob o botão borracha
+            {
+                var colorDialog = new ColorDialog();
+                if (colorDialog.ShowDialog() == DialogResult.OK)
+                {
+                    corBorracha = colorDialog.Color; // Seleciona a cor da borracha de acordo com a escolha do usuário
+                }
+            }
+            else
+            {
+                if(!flagApagar) // O operador ! antes irá virar o valor da flagApagar ao contrário, ou seja, se é true ficará false
+                {
+                    flagApagar = true;
+                    buttonBorracha.BackColor = corBorracha; // cor do botão será a mesma da borracha
+                }
+                else
+                {
+                    flagApagar = false; // Borracha não estará mais selecionada
+                    buttonBorracha.BackColor = Color.Black; // cor do botão voltará ao padrão
+                }
+            }
+        }
+
+        private void buttonSalvar_Click(object sender, EventArgs e)
+        {
+            var saveFileDialog = new SaveFileDialog(); // Janela para salvar arquivos
+            saveFileDialog.Filter = "Portable Network Graphics|.png|Arquivo JPEG|.jpeg"; // Atribuindo formatos de imagem para o usuário escolher
+            if(saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Definindo a extensão da imagem que iremos salvar
+                switch(saveFileDialog.FilterIndex)
+                {
+                    case 1:
+                        imagemASalvar.Save(saveFileDialog.FileName, ImageFormat.Png);
+                        break;
+                    case 2:
+                        imagemASalvar.Save(saveFileDialog.FileName, ImageFormat.Jpeg);
+                        break;
+                }
+            }
+        }
+
+        // Evento disparado sempre que o painel é redimensionado
+        private void panelPintura_Resize(object sender, EventArgs e)
+        {
+            // As linhas de código abaixo são necessárias pois ao redimensionar o painel
+            // o objeto graphics passa a ter dimensões diferentes, logo, precisamos atualizar 
+            // a referência da variável graphicsPainelPintura de forma a podermos desenhar
+            // nesse novo espaço.
+            // Também precisamos garantir que todo o desenho já feito pelo usuário não seja
+            // perdido ao aumentarmos o tamanho do painel de desenho, então precisamos 
+            // copiá-lo para a imagem a ser salva. Como não podemos redimensionar um
+            // objeto Image depois dele já ter sido instanciado, criamos uma imagem
+            // temporária e copiamos para ela tudo aquilo que já foi desenhado anteriormente
+            // através do método DrawImage(). Por fim, atualizamos as referências das variáveis
+            // imagemASalvar e graphicsImagemASalvar de forma tornarmos a imagem temporária
+            // a imagem a ser salva.
+
+            graphicsPainelPintura = panelPintura.CreateGraphics(); // Atualiza a referência do objeto graphics do painel
+            var imgTemp = new Bitmap(panelPintura.Width, panelPintura.Height); // Criamos uma imagem temporária
+            var graphicsImgTemp = Graphics.FromImage(imgTemp);
+            graphicsImgTemp.DrawImage(imagemASalvar, 0, 0); // Desenhamos a imagem antiga na imagem temporária - maneira de fazer uma cópia dos dados
+            imagemASalvar = imgTemp;
+            graphicsImagemASalvar = graphicsImgTemp;
+        }
+    }
+}
